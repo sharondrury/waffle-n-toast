fab1f278e870dfcccf74eff112958ec49f1447ad
Sharon Drury <98319755+sharondrury@users.noreply.github.com>
2026-02-17 09:10:56 +0000

    changes to index.js to get routing to work with gh-pages

--- waffle-n-toast/src/index.js
+++ waffle-n-toast/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './Routes';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import AppRoutes from "./Routes";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
-  </React.StrictMode>
+    </HashRouter>
+    {/* </BrowserRouter> */}
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function

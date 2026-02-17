cc0966dd307db7d6d4d6d70065ca13e6affcb938
Sharon Drury <98319755+sharondrury@users.noreply.github.com>
2026-02-17 16:51:05 +0000

    changed accordion to primereact

--- waffle-n-toast/src/Pages/Home/Components/Faq/Faq.jsx
+++ waffle-n-toast/src/Pages/Home/Components/Faq/Faq.jsx
@@ -1,35 +1,66 @@
 import "./Faq.scss";
 import { faqSection } from "../../../../data/data";
-import React from "react";
+
+import { Accordion, AccordionTab } from "primereact/accordion";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";  
 
 const Faq = () => {
-  const [people] = React.useState(faqSection);
+
+  const createDynamicTabs = () => {
+    return faqSection.map((faqSection) => {
+      return (
+        <AccordionTab
+          key={faqSection.id}
+          header={faqSection.question}
+          disabled={faqSection.disabled}
+
+        >
+          {faqSection.answer}
+        </AccordionTab>
+      );
+    });
+  };
 
   return (
-    <div className="faq-background">
-      <div className="faq-container">
-        <div>
-          <h2>Frequently asked question at Waffles and Toast Web Creatives</h2>
-        </div>
+  <div className="faq-background">
+    <div className="faq-container">
+      <div>
+        <h2>Frequently asked question at Waffles and Toast Web Creatives</h2>
+      </div>
 
-        <div className="accordion-container">
-          {people.map((person) => (
-            <div className="accordion" key={person.id}>
-              <div className="accordion-item">
-                <input type="checkbox" id={person.id} />
-                <label htmlFor={person.id} className="accordion-header">
-                  <span>{person.question}</span>
-                  <span className="icon">+</span>
-                </label>
-                <div className="accordion-content">
-                  <p>{person.answer}</p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+      <div className="accordion-container">
+        <Accordion className="accordion">{createDynamicTabs()}</Accordion>
       </div>
     </div>
+  </div>
+  
+    // <div className="faq-background">
+    //   <div className="faq-container">
+    //     <div>
+    //       <h2>Frequently asked question at Waffles and Toast Web Creatives</h2>
+    //     </div>
+
+    //     <div className="accordion-container">
+    //       {people.map((person) => (
+    //         <div className="accordion" key={person.id}>
+    //           <div className="accordion-item">
+    //             <input type="checkbox" id={person.id} />
+    //             <label htmlFor={person.id} className="accordion-header">
+    //               <span>{person.question}</span>
+    //               <span className="icon">+</span>
+    //             </label>
+    //             <div className="accordion-content">
+    //               <p>{person.answer}</p>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </div>
+
   );
 };
 

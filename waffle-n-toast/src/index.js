08f55194dc0915d247f0c4e8597a93258cce792e
Sharon Drury <98319755+sharondrury@users.noreply.github.com>
2026-02-20 10:09:49 +0000

    change from broweserrouter to hashrouter

--- waffle-n-toast/src/index.js
+++ waffle-n-toast/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import AppRoutes from './Routes';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
